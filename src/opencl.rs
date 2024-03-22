517bfa4b6ed480c5ca8980bd328b7aa712f08822
Toby Davis <pencilcaseman@gmail.com>
2024-03-22 03:44:11 +0000

    Minor bug fix

--- src/opencl.rs
+++ src/opencl.rs
@@ -1,5 +1,6 @@
 use crate::hasty_impl;
 
+/// Configure OpenCL -- Temporary function. This will be replaced soon
 pub fn configure_opencl(verbose: bool, ask: bool) {
     unsafe {
         crate::hasty_impl::configureOpenCL(verbose, ask);
